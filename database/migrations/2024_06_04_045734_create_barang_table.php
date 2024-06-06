35c4f84beae89c83bece729a20ce3c1a46dd4942
Didit Supriadi <elfidh17@gmail.com>
2024-06-06 20:38:35 +0800

    migration table barang dan log

---
+++ database/migrations/2024_06_04_045734_create_barang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->integer('kode_barang')->primary();
+            $table->char('nama_barang', 20);
+            $table->char('grade', 5);
+            $table->smallInteger('stok');
+            $table->timestamps(); // Optional: for created_at and updated_at columns
+
+            $table->unique(['kode_barang', 'nama_barang']); // Tambahkan unique constraint
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
